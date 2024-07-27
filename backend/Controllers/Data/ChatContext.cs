6fb498587a74b71fab99a3a7f30c4ef584d8ea74
Kalimabdulrauf <136961609+Kalimabdulrauf@users.noreply.github.com>
2024-07-27 21:37:12 +0300

    Create ChatContext.cs

---
+++ backend/Controllers/Data/ChatContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Models;
+
+namespace ChatApp.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
