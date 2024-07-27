580e62aa04b41f609e65f84943d5b51cbddbd283
Kalimabdulrauf <136961609+Kalimabdulrauf@users.noreply.github.com>
2024-07-27 21:50:59 +0300

    Create Message.cs

---
+++ backend/Controllers/Models/Message.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Text { get; set; }
+        public string Multimedia { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
