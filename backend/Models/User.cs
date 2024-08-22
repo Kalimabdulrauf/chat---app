6df035359985a0d4c468800bb86f4f79bbf97e4a
Kalimabdulrauf <136961609+Kalimabdulrauf@users.noreply.github.com>
2024-08-23 01:07:19 +0300

    Update User.cs

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,1 +1,11 @@
+namespace ChatApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
 
