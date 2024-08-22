b710c5231d4081146eec88ceb86152e6da328717
Kalimabdulrauf <136961609+Kalimabdulrauf@users.noreply.github.com>
2024-08-23 01:08:25 +0300

    Update ChatContext.cs

--- backend/Data/ChatContext.cs
+++ backend/Data/ChatContext.cs
@@ -1,1 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Models;
+
+namespace ChatApp.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
 
