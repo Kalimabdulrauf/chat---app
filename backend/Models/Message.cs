a2460b57af7f4d9efe2dfd71c77747ba97627ccd
Kalimabdulrauf <136961609+Kalimabdulrauf@users.noreply.github.com>
2024-08-23 01:07:52 +0300

    Update Message.cs

--- backend/Models/Message.cs
+++ backend/Models/Message.cs
@@ -1,1 +1,15 @@
+using System;
+
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Text { get; set; }
+        public string Multimedia { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
 
